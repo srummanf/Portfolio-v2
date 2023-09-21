b6f3a567fbab23d8bf397f5e93000242030e569a
Rumman <rummanfardeen4567@gmail.com>
2023-09-21 21:04:21 +0530

    🎉 first commit 🎉

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Srummanf | Portfolio',
+  description: 'Rumman is a Full Stack Dev with 1 year of experience!',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} bg-gray-50`}>{children}</body>
+    </html>
+  )
+}
